60ad7fd44d4b458b548866a964342b7070bea15d
Jean-Louis Fuchs <jean-louis.fuchs@adfinis-sygroup.ch>
2020-11-04 18:47:44 +0100

    Fix hypthesis warning about fixtures

--- manabi/token_test.py
+++ manabi/token_test.py
@@ -1,3 +1,4 @@
+import shutil
 from subprocess import PIPE, run
 
 import pytest
@@ -5,7 +6,7 @@ from branca import Branca
 from hypothesis import given
 from hypothesis.strategies import text
 
-from .conftest import branca_impl
+from .conftest import branca_impl, get_config, get_server_dir
 from .token import check_token, make_token
 from .util import fromstring
 
@@ -33,9 +34,12 @@ def test_make_token(tamper, expire, path, config):
         assert check_token(key, data, path, ttl) == check
 
 
+# hypothesis doesn't like fixtures anymore
+@pytest.mark.skipif(not shutil.which("cargo"), reason="needs rustc and cargo")
 @given(text(min_size=1, max_size=32))
-def test_other_impl_decode(config, string):
+def test_other_impl_decode(string):
     with branca_impl():
+        config = get_config(get_server_dir())
         string = string.encode("UTF-8")
         key = config["manabi"]["key"]
         f = Branca(fromstring(key))
