af88eccd8f2a5f554109a88624141d03261b91d8
Jean-Louis Fuchs <jean-louis.fuchs@adfinis-sygroup.ch>
2020-11-05 17:33:38 +0100

    Make ManabiAuthenticator an HTTPAuthenticator

--- manabi/__init__.py
+++ manabi/__init__.py
@@ -1,6 +1,25 @@
-from .util import tostring
+from wsgidav import http_authenticator
+
+from .auth import ManabiAuthenticator
+
+# Workaround an ugly hack in wsgidav
+http_authenticator.HTTPAuthenticator = ManabiAuthenticator
 
 
 def keygen():
+    from .util import tostring
+
     with open("/dev/random", "rb") as f:
         print(tostring(f.read(32)))
+
+
+def setup_log():
+    from wsgidav.util import init_logging
+
+    from .mock import get_config, get_server_dir
+
+    config = get_config(get_server_dir())
+    init_logging(config)
+
+
+setup_log()
