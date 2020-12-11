26c015c7c96aa120151aab6f41bef11328f205cc
Jean-Louis Fuchs <jean-louis.fuchs@adfinis-sygroup.ch>
2020-12-11 01:32:24 +0100

    Enable verbose logging in test server again

--- manabi/__main__.py
+++ manabi/__main__.py
@@ -1,8 +1,10 @@
 if __name__ == "__main__":
     # This needs dev-requirements
+    from .log import verbose_logging
     from .mock import get_config, get_server, get_server_dir
 
     config = get_config(get_server_dir())
     config["manabi"]["secure"] = False
+    verbose_logging()
     server = get_server(config)
     server.serve()
