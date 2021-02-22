c9d8f4b26106ef26635e628b07b5b3ecac480b6c
Jean-Louis Fuchs <jean-louis.fuchs@adfinis-sygroup.ch>
2021-02-22 16:23:58 +0100

    Create middleware that logs request and response headers

--- manabi/log.py
+++ manabi/log.py
@@ -1,4 +1,29 @@
-from wsgidav.util import init_logging  # type: ignore
+from functools import partial
+from typing import Any, Callable, Dict, List, Tuple
+
+from wsgidav.middleware import BaseMiddleware  # type: ignore
+from wsgidav.util import get_module_logger, init_logging  # type: ignore
+
+_logger = get_module_logger(__name__)
+
+
+class HeaderLogger(BaseMiddleware):
+    def logger(
+        self,
+        environ: Dict[str, Any],
+        start_response: Callable,
+        status: int,
+        headers: List[Tuple[str, str]],
+        exc_info=None,
+    ):
+        _logger.debug(f"Request environ: {environ}")
+        _logger.debug(f"Response headers: {dict(headers)}")
+        return start_response(status, headers, exc_info)
+
+    def __call__(
+        self, environ: Dict[str, Any], start_response: Callable
+    ) -> List[bytes]:
+        return self.next_app(environ, partial(self.logger, environ, start_response))
 
 
 def verbose_logging() -> None:
@@ -7,6 +32,7 @@ def verbose_logging() -> None:
         {
             "verbose": 5,
             "enable_loggers": [
+                "manabi.log",
                 "manabi.lock",
                 "lock_manager",
                 "lock_storage",
